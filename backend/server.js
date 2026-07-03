1db58b63d328df938c4f6628b14868f628a1373c
Tousif Razagit config --global user.email tousifraza9696@gmail.com <ahsanraza7117@gmail.com>
2026-07-03 21:30:58 +0530

    Update CORS for deployment

--- backend/server.js
+++ backend/server.js
@@ -13,10 +13,12 @@ const PORT = process.env.PORT || 3000;
 
 // middleware 
 app.use(express.json())
-app.use(cors({ 
-    origin:'http://localhost:5173',
-    credentials: true
-}))
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
 
 app.use('/api/v1/user',userRoute)
 app.use('/api/v1/product',productRoute)
